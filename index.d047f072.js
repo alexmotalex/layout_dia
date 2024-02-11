3f669e7efc90c0388f970b88d8258007f523b0fa
Alex <tokyopac@gmail.com>
2024-02-11 14:54:38 +0200

    make build

---
+++ index.d047f072.js
@@ -0,0 +1,3 @@
+var e=1;function l(l){var n=document.getElementsByClassName("js-slide");l>n.length&&(e=1),l<1&&(e=n.length);for(var s=0;s<n.length;s++)n[s].style.display="none";n[e-1].style.display="block"}l(1),l(e+=void 0);//# sourceMappingURL=index.d047f072.js.map
+
+//# sourceMappingURL=index.d047f072.js.map
